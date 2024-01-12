5396cdbb8ae83d3094e701019cac36999d868da5
CodeMaster7000 <abhyankar.advait19@gmail.com>
2024-01-12 23:30:21 +0000

    Update ChangeIPLocation.py

--- ChangeIPLocation.py
+++ ChangeIPLocation.py
@@ -5,13 +5,13 @@ from time import sleep
 codeList = ["TR", "US-C", "US", "US-W", "CA-W", "FR", "DE", "NL", "NO", "RO", "CA", "CH", "GB", "HK"]
 
 try:
-	os.system("Windscribe connect")
+	os.system("windscribe connect")
 	while True:
 		choiceCode = random.choice(codeList)
 		sleep(random.randrange(120, 300))
 		print("Changing IP address...")
-		os.system("Windscribe connect " + choiceCode)
+		os.system("windscribe connect " + choiceCode)
 
 except:
-	os.system("Windscribe disconnect")
+	os.system("windscribe disconnect")
 	print("An unexpected error occurred.")
